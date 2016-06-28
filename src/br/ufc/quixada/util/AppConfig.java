2b4e7e27e23eb56425d5d8fb8d708e51a166fa3a
Ederson Abreu <ederson.dimm@gmail.com>
2016-06-28 19:02:01 -0300

    Correção de bug no envio de e-mails

--- src/br/ufc/quixada/util/AppConfig.java
+++ src/br/ufc/quixada/util/AppConfig.java
@@ -5,18 +5,13 @@ import java.io.File;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
-
 import br.com.caelum.vraptor.events.VRaptorInitialized;
 
 @ApplicationScoped
 public class AppConfig {
 	
 	public static final String HOME = System.getProperty("user.home");
-	private SimpleEmail mailer;
 
-	@SuppressWarnings("deprecation")
 	public void config(@Observes VRaptorInitialized initialized){
 		File path = new File(HOME, "app-root");
 		path.mkdir();
@@ -26,22 +21,5 @@ public class AppConfig {
 				
 		File path3 = new File(path2.getAbsolutePath(), "uploads");
 		path3.mkdir();
-		
-		mailer = new SimpleEmail();
-		try {
-			mailer.setHostName("smtp.gmail.com");
-			mailer.setFrom("no-reply@infojornal.com","Sistema");
-			mailer.setAuthentication("", "");
-			mailer.setSmtpPort(587);
-			mailer.setSSL(true);
-			mailer.setTLS(true);
-		} catch (EmailException e) {
-			System.out.println("VERIFIQUE AS CONFIGURAÇÕES DO SMTP SERVER NA CLASSE:\nbr.ufc.quixada.util.AppConfig");
-			//e.printStackTrace();
-		}
-	}
-	
-	public SimpleEmail getMailer(){
-		return mailer;
 	}
 }
--- src/br/ufc/quixada/util/EmailSender.java
+++ src/br/ufc/quixada/util/EmailSender.java
@@ -1,7 +1,6 @@
 package br.ufc.quixada.util;
 
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
@@ -9,14 +8,19 @@ import org.apache.commons.mail.SimpleEmail;
 @RequestScoped
 public class EmailSender {
 	
-	@Inject private AppConfig system;
-	
+	@SuppressWarnings("deprecation")
 	public void enviar(String destino, String login, String novaSenha){
-		SimpleEmail mailer = system.getMailer();
 		try {
+			SimpleEmail mailer = new SimpleEmail();
+			mailer.setFrom("no-reply@infojornal.com","Sistema");
 			mailer.addTo(destino);
 			mailer.setSubject("Nova senha de acesso");
 			mailer.setMsg("Para acessar o sistema utilize:\n\nUsuario: "+login+"\n\nSenha: "+novaSenha);
+			mailer.setHostName("smtp.gmail.com");
+			mailer.setAuthentication("", "");
+			mailer.setSmtpPort(587);
+			mailer.setSSL(true);
+			mailer.setTLS(true);
 			mailer.send();
 		} catch (EmailException e) {
 			e.printStackTrace();
